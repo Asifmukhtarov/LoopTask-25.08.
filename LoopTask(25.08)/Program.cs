7d25e79e2b5467c0550ead54f977c3d1d08db704
Asifmukhtarov <muxtarovasif123@gmail.com>
2024-08-27 15:53:22 +0400

    Tasks was added

---
+++ LoopTask(25.08)/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Task 1
+int A=3;
+int B = 24;
+for (int i=A;i<B; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+*/
+
+
+//Task 2
+
+
+/*float num = 256;
+bool result = false;    
+for (float i = 2; i <= num; i *= 2)
+{
+    if (num / i == 1)
+    {
+
+        result = true;
+
+    }
+   
+
+}
+if (result)
+{
+    Console.WriteLine("2nin quvvetidir");
+}
+else
+{
+    Console.WriteLine("2nin quvveti deyil");
+}*/
